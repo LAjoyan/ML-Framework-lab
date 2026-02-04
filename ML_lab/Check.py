0eb0922c545a9be3cb7d98fa20e34a89e9e824e2
lilit900 <ajoyan.lilit@gmail.com>
2026-02-04 13:52:00 +0100

    style: apply code formatting

--- ML_lab/Check.py
+++ ML_lab/Check.py
@@ -7,27 +7,21 @@ import ipykernel
 
 def main():
 
-    print(f'''*****Python version***** 
+    print(f"""*****Python version***** 
     Python version: {sys.version.split()[0]}
-          ''')
-          
-          
-    print(f'''*****Package versions*****
+          """)
+
+    print(f"""*****Package versions*****
     torch: {torch.__version__}")
     scikit-learn: {sklearn.__version__}")
     pandas: {pd.__version__}")    
     ipykernel: {ipykernel.__version__} 
 
-''')      
-          
+""")
     print("\n--- Hardware / Accelerator ---")
     cuda_available = torch.cuda.is_available()
     print(f"CUDA available: {cuda_available}")
-          
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
-
-
