0ce9e1cc716344c2cedc71c29ee392635fb213cb
lilit900 <ajoyan.lilit@gmail.com>
2026-02-04 13:45:08 +0100

    feat: add package version check script

--- ML_lab/Check.py
+++ ML_lab/Check.py
@@ -1,12 +1,29 @@
 import sys
+import torch
+import sklearn
+import pandas as pd
+import ipykernel
 
 
 def main():
 
-    print(f"""*****Python version***** 
+    print(f'''*****Python version***** 
     Python version: {sys.version.split()[0]}
-          """)
+          ''')
+          
+          
+    print(f'''*****Package versions*****
+    torch: {torch.__version__}")
+    scikit-learn: {sklearn.__version__}")
+    pandas: {pd.__version__}")    
+    ipykernel: {ipykernel.__version__} 
+
+''')      
+          
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
+
+
+
