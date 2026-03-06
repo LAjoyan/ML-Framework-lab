4610c9fcf9d36d0360718e8621641bcfe02a447c
Josefin Lesley <josefin3647@gmail.com>
2026-03-06 21:37:02 +0100

    Changed the CIFAR-picture

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,16 +4,16 @@ import requests
 API_URL = "http://ml-app:8000"
 
 st.set_page_config(
-    page_title="CIFAR-10 App", 
+    page_title="CIFAR-10 App",
     layout="centered"
 )
 
 st.title("🏠 Home")
 
 with st.sidebar:
-    #st.title("⚙️ System")
+    # st.title("⚙️ System")
 
-    #st.subheader("⚙️ Health")
+    st.header("⚙️ Health")
 
     if st.button("Health Check"):
         try:
@@ -27,16 +27,12 @@ with st.sidebar:
 
     st.divider()
 
-    st.subheader("📦 Model Information")
+    st.header("📦 Model info")
 
     st.markdown("""
 **Framework:** ONNX Runtime
                 
 **Dataset:** CIFAR-10
-                
-**Image size:** 32×32 pixels
-                
-**Classes:** 10
                 
 **Expected accuracy:** ~73.85%
 """)
@@ -49,10 +45,15 @@ This application demonstrates an **image classification model trained on the CIF
 CIFAR-10 is a well-known benchmark dataset in computer vision consisting of **60,000 small color images (32×32 pixels)** divided into **10 different classes**.
 
 The model has been exported to the **ONNX format** and is served through a **FastAPI backend**, allowing efficient inference through **ONNX Runtime**.
+""")
 
-### Classes in CIFAR-10
-The model can classify the following objects:
+st.markdown("### Classes in CIFAR-10")
+st.markdown("The model can classify the following objects:")
 
+col1, col2 = st.columns([1, 0.8])
+
+with col1:
+    st.markdown("""
 - ✈️ Airplane  
 - 🚗 Automobile  
 - 🐦 Bird  
@@ -65,11 +66,11 @@ The model can classify the following objects:
 - 🚚 Truck  
 """)
 
-st.image(
-    "https://storage.googleapis.com/kaggle-media/competitions/kaggle/3649/media/cifar-10.png",
-    caption="Examples from the CIFAR-10 dataset",
-    use_container_width=True
-)
+with col2:
+    st.image(
+        "https://storage.googleapis.com/kaggle-media/competitions/kaggle/3649/media/cifar-10.png",
+        use_container_width=True
+    )
 
 st.markdown("""
 Use the **sidebar** to navigate to the prediction page where you can upload an image and test the model.
